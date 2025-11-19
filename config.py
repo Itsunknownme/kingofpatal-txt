71bfaa77dafa5ad5bd7946a7b08aaeb74c5dc23e
Itsunknownme <155176751+Itsunknownme@users.noreply.github.com>
2025-11-19 21:47:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-BOT_TOKEN=""
-API_HASH="" 
-API_ID=""
+import os
+from pyrogram import Client
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+bot = Client("my_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
+bot.run()
